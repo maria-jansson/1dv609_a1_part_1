136aa21cf35d559d599694395893fe114fea72bc
Maria Jansson <mj225er@student.lnu.se>
2025-11-26 20:08:12 +0100

    Add test for isCorrectFormat()

--- java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SSNHelperTest.java
+++ java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SSNHelperTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 
 public class SSNHelperTest {
   // static BuggySSNHelperAllowDayUpTo30 helper = new BuggySSNHelperAllowDayUpTo30();
-   static BuggySSNHelperAllowMonth0 helper = new BuggySSNHelperAllowMonth0();
-  // static BuggySSNHelperIncorrectFormat helper = new BuggySSNHelperIncorrectFormat();
-  // static BuggySSNHelperIncorrectFormatFalse helper = new BuggySSNHelperIncorrectFormatFalse();
+  // static BuggySSNHelperAllowMonth0 helper = new BuggySSNHelperAllowMonth0();
+  // static BuggySSNHelperIncorrectFormat helper = new BuggySSNHelperIncorrectFormat(); // Always true
+   static BuggySSNHelperIncorrectFormatFalse helper = new BuggySSNHelperIncorrectFormatFalse(); // Always false
   // static BuggySSNHelperMessyLuhn helper = new BuggySSNHelperMessyLuhn();
   // static BuggySSNHelperWrongLength helper = new BuggySSNHelperWrongLength();
 
@@ -21,5 +21,17 @@ public class SSNHelperTest {
   void isValidMonth_Should_Return_False_For_Incorrect_Input() {
     String month = "0";
     assertFalse(helper.isValidMonth(month));
+  }
+
+  @Test
+  void isCorrectFormat_Should_Return_True_For_Correct_Input() {
+    String input = "850624-2436";
+    assertTrue(helper.isCorrectFormat(input));
+  }
+
+  @Test
+  void isCorrectFormat_Should_Return_False_For_InCorrect_Input() {
+    String input = "8506-242436";
+    assertFalse(helper.isCorrectFormat(input));
   }
 }
