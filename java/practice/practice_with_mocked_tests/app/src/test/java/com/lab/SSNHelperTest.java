9e8c67b38457e2e3ddfbfb7e69c7d187adcaa12e
Maria Jansson <mj225er@student.lnu.se>
2025-11-26 20:11:17 +0100

    Refactor test names for consistency

--- java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SSNHelperTest.java
+++ java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SSNHelperTest.java
@@ -7,31 +7,36 @@ public class SSNHelperTest {
   // static BuggySSNHelperAllowDayUpTo30 helper = new BuggySSNHelperAllowDayUpTo30();
   // static BuggySSNHelperAllowMonth0 helper = new BuggySSNHelperAllowMonth0();
   // static BuggySSNHelperIncorrectFormat helper = new BuggySSNHelperIncorrectFormat(); // Always true
-   static BuggySSNHelperIncorrectFormatFalse helper = new BuggySSNHelperIncorrectFormatFalse(); // Always false
-  // static BuggySSNHelperMessyLuhn helper = new BuggySSNHelperMessyLuhn();
+  // static BuggySSNHelperIncorrectFormatFalse helper = new BuggySSNHelperIncorrectFormatFalse(); // Always false
+   static BuggySSNHelperMessyLuhn helper = new BuggySSNHelperMessyLuhn();
   // static BuggySSNHelperWrongLength helper = new BuggySSNHelperWrongLength();
 
   @Test
-  void isValidDay_Should_Return_True_For_Correct_Input() {
+  void isValidDay_Should_Return_True_For_Valid_Input() {
     String day = "31";
     assertTrue(helper.isValidDay(day));
   }
 
   @Test
-  void isValidMonth_Should_Return_False_For_Incorrect_Input() {
+  void isValidMonth_Should_Return_False_For_Invalid_Input() {
     String month = "0";
     assertFalse(helper.isValidMonth(month));
   }
 
   @Test
-  void isCorrectFormat_Should_Return_True_For_Correct_Input() {
+  void isCorrectFormat_Should_Return_True_For_Valid_Input() {
     String input = "850624-2436";
     assertTrue(helper.isCorrectFormat(input));
   }
 
   @Test
-  void isCorrectFormat_Should_Return_False_For_InCorrect_Input() {
+  void isCorrectFormat_Should_Return_False_For_Invalid_Input() {
     String input = "8506-242436";
     assertFalse(helper.isCorrectFormat(input));
+  }
+
+  @Test
+  void luhnIsCorrect_Should_Return_True_For_Valid_Input() {
+
   }
 }
