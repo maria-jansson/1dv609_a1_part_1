936a9d025108eea4dd0c4ab2c2a6eeae2cbfd087
Maria Jansson <mj225er@student.lnu.se>
2025-11-26 20:00:20 +0100

    Add test for isValidMonth()

--- java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SSNHelperTest.java
+++ java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SSNHelperTest.java
@@ -4,8 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class SSNHelperTest {
-  static BuggySSNHelperAllowDayUpTo30 helper = new BuggySSNHelperAllowDayUpTo30();
-  // static BuggySSNHelperAllowMonth0 helper = new BuggySSNHelperAllowMonth0();
+  // static BuggySSNHelperAllowDayUpTo30 helper = new BuggySSNHelperAllowDayUpTo30();
+   static BuggySSNHelperAllowMonth0 helper = new BuggySSNHelperAllowMonth0();
   // static BuggySSNHelperIncorrectFormat helper = new BuggySSNHelperIncorrectFormat();
   // static BuggySSNHelperIncorrectFormatFalse helper = new BuggySSNHelperIncorrectFormatFalse();
   // static BuggySSNHelperMessyLuhn helper = new BuggySSNHelperMessyLuhn();
@@ -15,5 +15,11 @@ public class SSNHelperTest {
   void isValidDay_Should_Return_True_For_Correct_Input() {
     String day = "31";
     assertTrue(helper.isValidDay(day));
+  }
+
+  @Test
+  void isValidMonth_Should_Return_False_For_Incorrect_Input() {
+    String month = "0";
+    assertFalse(helper.isValidMonth(month));
   }
 }
