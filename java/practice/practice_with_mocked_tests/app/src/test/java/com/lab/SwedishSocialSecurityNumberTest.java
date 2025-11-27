b2aba1a13646f05b5e5c6a8b28b670715182a0f1
Maria Jansson <mj225er@student.lnu.se>
2025-11-27 17:58:28 +0100

    Add test for custom bug class 1

--- java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SwedishSocialSecurityNumberTest.java
+++ java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SwedishSocialSecurityNumberTest.java
@@ -3,23 +3,92 @@ package com.lab;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class SwedishSocialSecurityNumberTest {
     
-    private SSNHelper helper;
+    // private SSNHelper helper;
     
+    // @BeforeEach
+    // public void setUp() {
+    //     helper = new SSNHelper();
+    // }
+    
+    // @Test
+    // public void shouldAcceptValidSSN() throws Exception {
+    //     SwedishSocialSecurityNumber ssn = new SwedishSocialSecurityNumber("900101-0017", helper);
+        
+    //     assertEquals("90", ssn.getYear());
+    //     assertEquals("01", ssn.getMonth());
+    //     assertEquals("01", ssn.getDay());
+    //     assertEquals("0017", ssn.getSerialNumber());
+    // }
+
+    private SSNHelper mockHelper;
+
     @BeforeEach
     public void setUp() {
-        helper = new SSNHelper();
+        mockHelper = mock(SSNHelper.class);
+        when(mockHelper.isCorrectLength(anyString())).thenReturn(true);
+        when(mockHelper.isCorrectFormat(anyString())).thenReturn(true);
+        when(mockHelper.isValidMonth(anyString())).thenReturn(true);
+        when(mockHelper.isValidDay(anyString())).thenReturn(true);
+        when(mockHelper.luhnIsCorrect(anyString())).thenReturn(true);
+    }
+
+    @Test
+    void constructor_Should_Check_For_Input_Length() throws Exception {
+        String validSSN = "850913-2398";
+
+        // SwedishSocialSecurityNumber sut = new SwedishSocialSecurityNumber(validSSN, mockHelper);
+        BuggySwedishSocialSecurityNumberNoLenCheck sut = new BuggySwedishSocialSecurityNumberNoLenCheck(validSSN, mockHelper);
+
+        verify(mockHelper).isCorrectLength(validSSN);
+    }
+
+    @Test
+    void constructor_Should_Check_Input_Against_Luhn_Algorithm() throws Exception {
+        String validSSN = "850913-2398";
+
+        // SwedishSocialSecurityNumber sut = new SwedishSocialSecurityNumber(validSSN, mockHelper);
+        BuggySwedishSocialSecurityNumberNoLuhn sut = new BuggySwedishSocialSecurityNumberNoLuhn(validSSN, mockHelper);
+        
+        verify(mockHelper).luhnIsCorrect(validSSN);
+    }
+
+    @Test
+    void constructor_Should_Trim_Input() throws Exception {
+        String ssnWithWhitespace = "850913-2398 ";
+
+        // SwedishSocialSecurityNumber sut = new SwedishSocialSecurityNumber(ssnWithWhitespace, mockHelper);
+        BuggySwedishSocialSecurityNumberNoTrim sut = new BuggySwedishSocialSecurityNumberNoTrim(ssnWithWhitespace, mockHelper);
+
+        String expected = "850913-2398";
+        String actual = sut.getSSN();
+
+        assertEquals(expected, actual);
     }
     
     @Test
-    public void shouldAcceptValidSSN() throws Exception {
-        SwedishSocialSecurityNumber ssn = new SwedishSocialSecurityNumber("900101-0017", helper);
-        
-        assertEquals("90", ssn.getYear());
-        assertEquals("01", ssn.getMonth());
-        assertEquals("01", ssn.getDay());
-        assertEquals("0017", ssn.getSerialNumber());
+    void getYear_Should_Return_Correct_Numbers_For_Year() throws Exception {
+        String validSSN = "850913-2398";
+
+        // SwedishSocialSecurityNumber sut = new SwedishSocialSecurityNumber(validSSN, mockHelper);
+        BuggySwedishSocialSecurityNumberWrongYear sut = new BuggySwedishSocialSecurityNumberWrongYear(validSSN, mockHelper);
+
+        String expected = "85";
+        String actual = sut.getYear();
+
+        assertEquals(expected, actual);
+    }
+    
+    @Test
+    void constructor_Should_Check_For_Correct_Format() throws Exception {
+        String ssnIncorrectHyphen = "8509-132398";
+
+        // SwedishSocialSecurityNumber sut = new SwedishSocialSecurityNumber(validSSN, mockHelper);
+        MyCustomBug1 sut = new MyCustomBug1(ssnIncorrectHyphen, mockHelper);
+
+        verify(mockHelper).isCorrectFormat(ssnIncorrectHyphen);
     }
 }
