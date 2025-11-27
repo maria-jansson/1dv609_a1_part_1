72f883234b9ea8be78e90631d51e8e90863c670f
Maria Jansson <mj225er@student.lnu.se>
2025-11-27 09:37:07 +0100

    Refactor SSNHelperTest to use consistent variable naming for the SUT

--- java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SSNHelperTest.java
+++ java/practice/practice_with_mocked_tests/app/src/test/java/com/lab/SSNHelperTest.java
@@ -4,53 +4,53 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class SSNHelperTest {
-  static SSNHelper helper = new SSNHelper();
-  // static BuggySSNHelperAllowDayUpTo30 helper = new BuggySSNHelperAllowDayUpTo30();
-  // static BuggySSNHelperAllowMonth0 helper = new BuggySSNHelperAllowMonth0();
-  // static BuggySSNHelperIncorrectFormat helper = new BuggySSNHelperIncorrectFormat(); // Always true
-  // static BuggySSNHelperIncorrectFormatFalse helper = new BuggySSNHelperIncorrectFormatFalse(); // Always false
-  // static BuggySSNHelperMessyLuhn helper = new BuggySSNHelperMessyLuhn();
-  // static BuggySSNHelperWrongLength helper = new BuggySSNHelperWrongLength();
+  static SSNHelper sut = new SSNHelper();
+  // static BuggySSNHelperAllowDayUpTo30 sut = new BuggySSNHelperAllowDayUpTo30();
+  // static BuggySSNHelperAllowMonth0 sut = new BuggySSNHelperAllowMonth0();
+  // static BuggySSNHelperIncorrectFormat sut = new BuggySSNHelperIncorrectFormat(); // Always true
+  // static BuggySSNHelperIncorrectFormatFalse sut = new BuggySSNHelperIncorrectFormatFalse(); // Always false
+  // static BuggySSNHelperMessyLuhn sut = new BuggySSNHelperMessyLuhn();
+  // static BuggySSNHelperWrongLength sut = new BuggySSNHelperWrongLength();
 
   @Test
   void isValidDay_Should_Return_True_For_Valid_Input() {
     String validDay = "31";
-    boolean actual = helper.isValidDay(validDay);
+    boolean actual = sut.isValidDay(validDay);
     assertTrue(actual);
   }
 
   @Test
   void isValidMonth_Should_Return_False_For_Invalid_Input() {
     String invalidMonth = "0";
-    boolean actual = helper.isValidMonth(invalidMonth);
+    boolean actual = sut.isValidMonth(invalidMonth);
     assertFalse(actual);
   }
 
   @Test
   void isCorrectFormat_Should_Return_True_For_Valid_Input() {
     String validSSN = "850913-2398";
-    boolean actual = helper.isCorrectFormat(validSSN);
+    boolean actual = sut.isCorrectFormat(validSSN);
     assertTrue(actual);
   }
 
   @Test
   void isCorrectFormat_Should_Return_False_For_Invalid_Input() {
     String ssnHyphenWrongPlace = "8509-132398";
-    boolean actual = helper.isCorrectFormat(ssnHyphenWrongPlace);
+    boolean actual = sut.isCorrectFormat(ssnHyphenWrongPlace);
     assertFalse(actual);
   }
 
   @Test
   void luhnIsCorrect_Should_Return_True_For_Valid_Input() {
     String validSSN = "850913-2398";
-    boolean actual = helper.luhnIsCorrect(validSSN);
+    boolean actual = sut.luhnIsCorrect(validSSN);
     assertTrue(actual);
   }
 
   @Test
   void isCorrectLength_Should_Return_False_For_Invalid_Input() {
     String ssnTooLong = "850913-23987";
-    boolean actual = helper.isCorrectLength(ssnTooLong);
+    boolean actual = sut.isCorrectLength(ssnTooLong);
     assertFalse(actual);
   }
 }
